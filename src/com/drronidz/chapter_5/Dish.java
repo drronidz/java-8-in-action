c1420d93527e1601ba624550345c46d306fd7d5e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2023-03-26 19:50:23 +0200

    Filtering with a predicate. closes #22

---
+++ src/com/drronidz/chapter_5/Dish.java
@@ -0,0 +1,53 @@
+package com.drronidz.chapter_5;
+
+/*
+PROJECT NAME : java-8-in-action
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 3/24/2023 2:13 PM
+*/
+
+public class Dish {
+
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public enum Type { MEAT, FISH, OTHER }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                ", vegetarian=" + vegetarian +
+                ", calories=" + calories +
+                ", type=" + type +
+                '}';
+    }
+}
+
+
