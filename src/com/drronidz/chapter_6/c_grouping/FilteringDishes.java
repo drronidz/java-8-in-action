c07aa40054f7b6c8b53a44acbfe2ddd28ff2a1c8
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2023-04-15 17:35:11 +0200

    Classification of an item in the stream during the grouping process. #31

---
+++ src/com/drronidz/chapter_6/c_grouping/FilteringDishes.java
@@ -0,0 +1,50 @@
+package com.drronidz.chapter_6.c_grouping;
+
+/*
+PROJECT NAME : java-8-in-action
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 4/15/2023 5:26 PM
+*/
+
+import com.drronidz.chapter_5.Dish;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.groupingBy;
+
+public class FilteringDishes {
+
+    /** Grouping **/
+    public static final List<Dish> menu = asList(
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 400, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH)
+    );
+
+    public static void main(String[] args) {
+        Map<Dish.Type, List<Dish>> dishesByType =
+                menu.stream().collect(groupingBy(Dish::getType));
+
+        // Classification of an item in the stream during the grouping process
+        Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = menu.stream().collect(
+                groupingBy(dish -> {
+                 if (dish.getCalories() <= 400) return CaloricLevel.DIET;
+                 else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+                 else return CaloricLevel.FAT;
+                }));
+    }
+
+    public enum CaloricLevel {
+        DIET, NORMAL, FAT
+    }
+}
